8018d1cc61c8d5b4662480ab0d114e646da6216a
gyovani19 <gyovani.santos@dcomp.ufs.br>
2024-02-08 14:23:14 -0300

    estrutura principal

--- netvak/src/main.tsx
+++ netvak/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './pages/Main.tsx'
+
+import GlobalStyle from './styles/global.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <GlobalStyle>
+    
+    </GlobalStyle>
+    <App/>
+    </React.StrictMode>,
 )
